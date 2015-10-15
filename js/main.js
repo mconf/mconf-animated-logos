fb0afacc200d01ff8ae70fe65659a98101a6bb38
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-10-15 13:57:07 -0300

    First version, 7 logos with different animations

---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(function() {
+    var animationEndEvent = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+    $("#mconf-logo4").on(animationEndEvent, function() {
+        if ($(this).hasClass("rollIn")) {
+            $(this).removeClass("rollIn");
+            $(this).addClass("rollOut");
+        } else {
+            $(this).removeClass("rollOut");
+            $(this).addClass("rollIn");
+        }
+    });
+    $("#mconf-logo5").on(animationEndEvent, function() {
+        if ($(this).hasClass("zoomIn")) {
+            $(this).removeClass("zoomIn");
+            $(this).addClass("zoomOut");
+        } else {
+            $(this).removeClass("zoomOut");
+            $(this).addClass("zoomIn");
+        }
+    });
+    $("#mconf-logo6").on(animationEndEvent, function() {
+        if ($(this).hasClass("flipInY")) {
+            $(this).removeClass("flipInY");
+            $(this).addClass("flipOutY");
+        } else {
+            $(this).removeClass("flipOutY");
+            $(this).addClass("flipInY");
+        }
+    });
+    $("#mconf-logo7").on(animationEndEvent, function() {
+        if ($(this).hasClass("fadeIn")) {
+            $(this).removeClass("fadeIn");
+            $(this).addClass("fadeOut");
+        } else {
+            $(this).removeClass("fadeOut");
+            $(this).addClass("fadeIn");
+        }
+    });
+    $("#mconf-logo1").on(animationEndEvent, function() {
+        var $logo = $("#mconf-logo1");
+        $logo.removeClass("rubberBand");
+        setTimeout(function() { $logo.addClass("rubberBand"); }, 50);
+    });
+    $("#mconf-logo2").on(animationEndEvent, function() {
+        var $logo = $("#mconf-logo2");
+        $logo.removeClass("flip");
+        setTimeout(function() { $logo.addClass("flip"); }, 50);
+    });
+    $("#mconf-logo3").on(animationEndEvent, function() {
+        var $logo = $("#mconf-logo3");
+        $logo.removeClass("rubberBand");
+        setTimeout(function() { $logo.addClass("rubberBand"); }, 50);
+    });
+});
